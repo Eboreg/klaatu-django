db6c646dc7804996ed19187cce097c3b697ac471
Robert Huselius <robert@huseli.us>
2022-09-16 23:34:37 +0200

    Added generic SortableStorage class

--- groplay/storage.py
+++ groplay/storage.py
@@ -1,9 +1,9 @@
 import operator
 from enum import Enum, auto
 from pathlib import Path
-from typing import Collection, List, Tuple, Union
+from typing import Collection, Generic, List, Tuple, TypeVar, Union
 
-from django.core.files.storage import FileSystemStorage as BaseFileSystemStorage
+from django.core.files.storage import FileSystemStorage as BaseFileSystemStorage, Storage
 
 
 class SortKey(Enum):
@@ -16,6 +16,7 @@ class SortKey(Enum):
 
 SortKeyWithOrder = Tuple[SortKey, bool]
 SortKeyArg = Collection[Union[SortKey, SortKeyWithOrder]]
+_T = TypeVar("_T", bound=object)
 
 
 def normalize_sort_keys(keys: SortKeyArg) -> List[SortKeyWithOrder]:
@@ -28,8 +29,8 @@ def normalize_sort_keys(keys: SortKeyArg) -> List[SortKeyWithOrder]:
     return ret
 
 
-class FileSystemStorage(BaseFileSystemStorage):
-    def sort_by(self, files: Collection[Path], keys: SortKeyArg) -> List[Path]:
+class SortableStorage(Storage, Generic[_T]):
+    def sort_by(self, files: Collection[_T], keys: SortKeyArg) -> List[_T]:
         nkeys = normalize_sort_keys(keys)
         reverse_all = False
 
@@ -37,28 +38,42 @@ class FileSystemStorage(BaseFileSystemStorage):
             if key == SortKey.NAME:
                 reverse_all = reverse
 
-        def sort_value(p: Path) -> List[Union[str, int, float]]:
-            stat = p.stat()
-            result: List[Union[str, int, float]] = []
-            for (key, reverse) in nkeys:
-                reverse = operator.xor(reverse, reverse_all)
-                multiplier = -1 if reverse else 1
-                if key == SortKey.NAME:
-                    result.append(p.name)
-                elif key == SortKey.ISDIR:
-                    result.append(operator.xor(p.is_dir(), reverse))
-                elif key == SortKey.SIZE:
-                    if p.is_dir():
-                        result.append(0)
-                    else:
-                        result.append(stat.st_size * multiplier)
-                elif key == SortKey.CTIME:
-                    result.append(stat.st_ctime * multiplier)
-                elif key == SortKey.MTIME:
-                    result.append(stat.st_mtime * multiplier)
-            return result
+        return sorted(files, key=lambda f: self.get_sort_value(f, nkeys, reverse), reverse=reverse_all)
 
-        return sorted(files, key=sort_value, reverse=reverse_all)
+    def get_sort_value(
+        self,
+        file: _T,
+        nkeys: List[SortKeyWithOrder],
+        reverse_all: bool
+    ) -> List[Union[str, int, float]]:
+        raise NotImplementedError("SortableStorage subclasses must implement get_sort_value().")
+
+    def list_recursive(self, path: str = "", sort: SortKeyArg = []) -> List[str]:
+        """Recursively lists files in `path`, ordered by `sort`."""
+        raise NotImplementedError("SortableStorage subclasses must implement list_recursive().")
+
+
+class FileSystemStorage(SortableStorage[Path], BaseFileSystemStorage):
+    def get_sort_value(self, file, nkeys, reverse_all):
+        stat = file.stat()
+        result: List[Union[str, int, float]] = []
+        for (key, reverse) in nkeys:
+            reverse = operator.xor(reverse, reverse_all)
+            multiplier = -1 if reverse else 1
+            if key == SortKey.NAME:
+                result.append(file.name)
+            elif key == SortKey.ISDIR:
+                result.append(operator.xor(file.is_dir(), reverse))
+            elif key == SortKey.SIZE:
+                if file.is_dir():
+                    result.append(0)
+                else:
+                    result.append(stat.st_size * multiplier)
+            elif key == SortKey.CTIME:
+                result.append(stat.st_ctime * multiplier)
+            elif key == SortKey.MTIME:
+                result.append(stat.st_mtime * multiplier)
+        return result
 
     def recurse(self, path: Union[Path, str], sort: SortKeyArg = []) -> List[Path]:
         """
@@ -74,7 +89,6 @@ class FileSystemStorage(BaseFileSystemStorage):
                 files.append(entry)
         return self.sort_by(files, sort)
 
-    def list_recursive(self, path="", sort: SortKeyArg = []) -> List[str]:
-        """Recursively lists files in `path`, ordered by `sort`."""
+    def list_recursive(self, path="", sort=[]):
         full_path = self.path(path)
         return [str(p)[len(full_path):].strip("/") for p in self.recurse(full_path, sort)]
